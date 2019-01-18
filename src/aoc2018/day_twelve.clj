d6045fbfb09309aae743780ada67f001c9c759c0
John Flinchbaugh <john@hjsoft.com>
2019-01-18 01:55:36 -0500

    day 12 part 1

---
+++ src/aoc2018/day_twelve.clj
@@ -0,0 +1,101 @@
+(ns aoc2018.day-twelve
+  (:require [clojure.string :as str])
+)
+
+(def initial-state "###......#.#........##.###.####......#..#####.####..#.###..#.###.#..#..#.#..#..#.##...#..##......#.#")
+
+(def transitions
+  "
+    ..#.# => .
+    #..## => #
+    ..### => .
+    ###.# => .
+    #...# => #
+    ###.. => #
+    .##.# => #
+    #..#. => #
+    #.##. => #
+    ####. => .
+    .#.## => #
+    ...#. => .
+    .#..# => #
+    .###. => .
+    ##..# => #
+    .##.. => #
+    .#### => #
+    .#.#. => #
+    ##### => .
+    #.#.# => #
+    ...## => #
+    ..##. => .
+    ....# => .
+    ##... => .
+    ##.#. => #
+    ..#.. => #
+    ..... => .
+    ##.## => .
+    #.### => .
+    #.#.. => .
+    .#... => #
+    #.... => .
+  "
+)
+
+(defn to-flags
+  "map the notation string into flags"
+  [s]
+  (->>
+    s
+    (map #(= \# %))
+    (into [])
+  )
+)
+
+(let
+  [
+    gens 20
+    mappings (as->
+      transitions v
+      (str/trim v)
+      (str/split v #"\n")
+      (map str/trim v)
+      (map #(str/split % #" => ") v)
+      (map (fn [[i r]] [(to-flags i) (first (to-flags r))]) v)
+      (into {} v)
+    )
+    initial (to-flags initial-state)
+    initial-with-room (concat
+      (repeat gens false)
+      [false false]
+      initial
+      [false false]
+      (repeat gens false)
+    )
+  ]
+  (->>
+    (loop
+      [
+        g gens
+        state initial-with-room
+      ]
+      (if (zero? g)
+        (vec state)
+        (recur
+          (dec g)
+          (as->
+            state v
+            (count v)
+            (range v)
+            (map inc v)
+            (reverse v)
+            (map #(take 5 (take-last % state)) v)
+            (map #(or (mappings %) false) v)
+            (concat [false false] v)
+          )
+        )
+      )
+    )
+    (map (fn [v f] (if f v 0)) (map #(- % 2 gens) (range)))
+    (reduce +)
+  )
+)
