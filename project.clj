f3ce86ca5ac6a549d129e24cc1ade2d79e98c122
John Flinchbaugh <john@hjsoft.com>
2019-06-22 23:08:53 -0400

    upgrade to clojure 1.10.1

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.10.1-beta2"]
+                 Unstaged changes (2)
+                 [org.clojure/clojure "1.10.1"]
   ]
 )
